24757a7445e3beba5e76ac30a6dee27b2042c4fa
ceci-00 <ceci.ga003@gmail.com>
2024-05-26 20:20:50 -0500

    created methods for saving content to the database and getting content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,5 @@
 import { openDB } from 'idb';
-
+// method that initializes the database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// method that saves content to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const todosDb = await openDB('jate', 1);
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
   console.log('Data saved to the database', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// method that gets content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const todosDb = await openDB('jate', 1);
